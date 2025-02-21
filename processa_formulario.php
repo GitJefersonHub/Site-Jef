59778ac7d4c597e365fddff0b23c56451c3328bf
“GitJefersonHub” <jefersonalves.ti@gmail.com>
2025-02-20 21:27:11 -0300

    novosite

--- processa_formulario.php
+++ processa_formulario.php
@@ -1,34 +1,15 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Verifica o método da requisição
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode(["message" => "Método não permitido"]);
+    exit;
+}
+
 // Configurações do banco de dados
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "meu_banco_de_dados";
-
-// Cria a conexão com o banco de dados
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verifica a conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
-
-// Coleta os dados do formulário
-$nome = htmlspecialchars($_POST['nome']);
-$email = htmlspecialchars($_POST['email']);
-$telefone = htmlspecialchars($_POST['telefone']);
-$mensagem = htmlspecialchars($_POST['mensagem']);
-
-// Prepara a consulta SQL para inserir os dados
-$sql = "INSERT INTO contatos (nome, email, telefone, mensagem) VALUES ('$nome', '$email', '$telefone', '$mensagem')";
-
-// Executa a consulta SQL
-if ($conn->query($sql) === TRUE) {
-    echo "Dados cadastrados com sucesso!";
-} else {
-    echo "Erro: " . $sql . "<br>" . $conn->error;
-}
-
-// Fecha a conexão com o banco de dados
-$conn->close();
-?>
+$dbname = "site-jef"; // Atualize para o nome do seu banco de dados[_{{{CITATION{{{_1{](https://github.com/emersonbrambila/zerodengue/tree/f41435a2d376c9caa347f0557c422bb317e52891/cadastro.php)[_{{{CITATION{{{_2{](https://github.com/LucasBlock/pramimechocolate/tree/252c6f931c2b5d1f6d0880e78006ab2dd93834ad/resources%2Fviews%2Fwelcome.blade.php)[_{{{CITATION{{{_3{](https://github.com/irzanaldi/Sistem_pakar_hidroponik/tree/30ebd437e711deb7bfc9fe432726b8cbb1c7f66d/resources%2Fviews%2Flayout%2FindexLayout.blade.php)[_{{{CITATION{{{_4{](https://github.com/rajerr/Rangba-Ouedane-Gerry-Oreste/tree/78ffde359091ba29ae46a6894bdef89cccef8d07/Mini_Projet%2Fpages%2Fheader.php)
